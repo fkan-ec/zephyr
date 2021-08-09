05dfe9d21396e804e42168165cf026cbd7d89fd8
Saurabh Karajgaonkar <saurabh.karajgaonkar@edgeq.io>
2021-08-09 17:31:44 +0530

    drivers: misc: Add support for multiple instances of raptor2 doorbell
    - This patch modifies the raptor2 doorbell driver to handle multiple
      instances of doorbell devices defined in device tree.
    
    Signed-off-by: Saurabh Karajgaonkar <saurabh.karajgaonkar@edgeq.io>

--- drivers/misc/raptor2/raptor2_doorbell.c
+++ drivers/misc/raptor2/raptor2_doorbell.c
@@ -1,53 +1,71 @@
+#define DT_DRV_COMPAT raptor2_doorbell
+
 #include <kernel.h>
 #include <device.h>
 #include <devicetree.h>
 
-#define DOORBELL	DT_NODELABEL(doorbell0)
+typedef void (*irq_config_func_t)(const struct device *dev);
 
 struct doorbell_data {
 	uint32_t isr_count;
 };
 
 struct doorbell_cfg {
+	uint32_t irq;
 	uint64_t mem_base;
+	irq_config_func_t irq_config_func;
 };
 
 void doorbell_isr(struct device *dev)
 {
 	struct doorbell_data *data;
+	struct doorbell_cfg *config;
 
 	data = (struct doorbell_data *)dev->data;
+	config = (struct doorbell_cfg *)dev->config;
+
 	data->isr_count++;
-	printk("Doorbell ISR triggered %d\n", data->isr_count);
+	printk("Doorbell ISR triggered %d for irq %d\n", data->isr_count, config->irq);
 }
 
 static int doorbell_init(const struct device *dev)
 {
-	IRQ_CONNECT(DT_IRQN(DOORBELL),
-		    DT_IRQ(DOORBELL, priority),
-		    doorbell_isr,
-		    DEVICE_DT_GET(DOORBELL),
-		    0);
-	irq_enable(DT_IRQN(DOORBELL));
+	struct doorbell_cfg *config;
+
+	config = (struct doorbell_cfg *)dev->config;
+
+	config->irq_config_func(dev);
 
 	return 0;
 }
 
 /* Device instantiation */
-static struct doorbell_data doorbell_data_0 = {
-	.isr_count = 0,
-};
 
-static struct doorbell_cfg doorbell_cfg_0 = {
-	.mem_base = DT_REG_ADDR(DOORBELL),
-};
+#define RAPTOR2_DOORBELL_INIT(n) \
+	static void doorbell_irq_config_func_##n(const struct device *dev) \
+	{ \
+		IRQ_CONNECT(DT_INST_IRQN(n), \
+		    DT_INST_IRQ(n, priority), \
+		    doorbell_isr, \
+		    DEVICE_DT_INST_GET(n), \
+		    0); \
+		irq_enable(DT_INST_IRQN(n)); \
+	} \
+	static struct doorbell_data doorbell_data_##n = { \
+		.isr_count = 0, \
+	}; \
+	static struct doorbell_cfg doorbell_cfg_##n = { \
+		.irq = DT_INST_IRQN(n), \
+		.mem_base = DT_INST_REG_ADDR(n), \
+		.irq_config_func = doorbell_irq_config_func_##n, \
+	}; \
+	DEVICE_DT_INST_DEFINE(n, \
+			doorbell_init, \
+			device_pm_control_nop, \
+			&doorbell_data_##n, \
+			&doorbell_cfg_##n, \
+			SMP, \
+			50, \
+			NULL);
 
-DEVICE_DT_DEFINE(DOORBELL, \
-		doorbell_init, \
-		device_pm_control_nop, \
-		&doorbell_data_0, \
-		&doorbell_cfg_0, \
-		SMP, \
-		50, \
-		NULL);
-
+DT_INST_FOREACH_STATUS_OKAY(RAPTOR2_DOORBELL_INIT)
