88f20ab64d836aea065ff9a54d6d66a6bcb7319b
Saurabh Karajgaonkar <saurabh.karajgaonkar@edgeq.io>
2021-10-19 14:32:46 +0530

    build.sh: Update build.sh for raptor2 A0 PPU build
    - Default build is for A0 PPU cores only
    
    Signed-off-by: Saurabh Karajgaonkar <saurabh.karajgaonkar@edgeq.io>

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 rm -rf build
-echo "Building for protium"
-west build -b raptor2_protium samples/subsys/shell/shell_module/
+echo "Building for A0 board with PPU cores only"
+west build -b raptor2_a0_ppu samples/subsys/shell/shell_module/
+#echo "Building for protium"
+#west build -b raptor2_protium samples/subsys/shell/shell_module/
 #echo "Building for SoCFM"
 #west build -b raptor2_socfm samples/subsys/shell/shell_module/
